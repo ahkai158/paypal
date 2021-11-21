a418d70bfd310493d7e73f1f62d7a67f2d0eb680
ahkai158 <94775069+ahkai158@users.noreply.github.com>
2021-11-21 13:41:57 +0800

    Add files via upload

---
+++ checkout.js
@@ -0,0 +1,35 @@
+
+const express = require('express');
+const router = express.Router();
+const braintree = require('braintree');
+
+router.post('/', (req, res, next) => {
+  const gateway = new braintree.BraintreeGateway({
+    environment: braintree.Environment.Sandbox,
+    // Use your own credentials from the sandbox Control Panel here
+    merchantId: 'wq236cq9kfw4dmfh',
+    publicKey: 'tsrv4vrh72h2djpf',
+    privateKey: '611f8c844ee72b7118a4d6c1ecfd8442'
+  });
+
+  // Use the payment method nonce here
+  const nonceFromTheClient = req.body.paymentMethodNonce;
+  // Create a new transaction for $10
+  const newTransaction = gateway.transaction.sale({
+    amount: '15.00',
+    paymentMethodNonce: nonceFromTheClient,
+    options: {
+      // This option requests the funds from the transaction
+      // once it has been authorized successfully
+      submitForSettlement: true
+    }
+  }, (error, result) => {
+      if (result) {
+        res.send(result);
+      } else {
+        res.status(500).send(error);
+      }
+  });
+});
+
+module.exports = router;
